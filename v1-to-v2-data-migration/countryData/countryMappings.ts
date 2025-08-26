b36e1b313e18671fc26498e7e408115d0e0e9c93
euanmillar <euanmillar77@gmail.com>
2025-08-26 18:16:47 +0530

    Added mappings for Somalia

--- v1-to-v2-data-migration/countryData/countryMappings.ts
+++ v1-to-v2-data-migration/countryData/countryMappings.ts
@@ -6,4 +6,30 @@
     {v1Field.customQuestionMappingId}: {v2Field.id}
 */
 
-export const COUNTRY_FIELD_MAPPINGS = {}
+export const COUNTRY_FIELD_MAPPINGS = {
+  'birth.child-view-group.iD': 'child.nid',
+  'birth.child-view-group.countryPrimaryChild': 'child.address',
+  'birth.child-view-group.statePrimaryChild': 'child.address',
+  'birth.child-view-group.districtPrimaryChild': 'child.address',
+  'birth.child-view-group.cityPrimaryChild': 'child.address',
+  'birth.child-view-group.addressLine1PrimaryChild': 'child.address',
+  'birth.child-view-group.addressLine2PrimaryChild': 'child.address',
+  'birth.child-view-group.addressLine3PrimaryChild': 'child.address',
+  'birth.child-view-group.postalCodePrimaryChild': 'child.address',
+  'birth.child-view-group.internationalCityPrimaryChild': 'child.address',
+  'birth.child-view-group.birthAttendantName': 'child.attendantName',
+  'birth.child-view-group.birthAttendantId': 'child.attedantAtBirthId',
+  'birth.informant-view-group.informantID': 'informant.nid',
+  'birth.mother-view-group.iD': 'mother.nid',
+  'birth.father-view-group.iD': 'father.nid',
+  'birth.documents-view-group.uploadDocOther': 'documents.proofOther',
+  'death.deceased-view-group.placeOfBirth': 'deceased.placeOfBirth',
+  'death.deceased-view-group.birthRegNo': 'deceased.brn',
+  'death.deceased-view-group.deceasedID': 'deceased.nid',
+  'death.deceased-view-group.occupation': 'deceased.occupation',
+  'death.death-event-details.timeOfDeath': 'eventDetails.timeOfDeath',
+  'death.informant-view-group.informantID': 'informant.nid',
+  'death.informant-view-group.informantType': 'informant.informantType',
+  'death.documents-view-group.uploadDocForInformant':
+    'documents.proofOfInformant',
+}
--- v1-to-v2-data-migration/countryData/countryResolvers.ts
+++ v1-to-v2-data-migration/countryData/countryResolvers.ts
@@ -1,1 +1,59 @@
-export const countryResolver = {}
+import { getCustomField, getIdentifier } from "./resolverUtils.ts";
+
+function convertChildAddress(data, address) {
+  if (!address) {
+    return null;
+  }
+
+  const country = getCustomField(
+    data,
+    "birth.child.child-view-group.countryPrimaryChild"
+  );
+  const international = country !== "FAR"; //This doesn't work for other countries
+  if (international) {
+    return {
+      addressType: "INTERNATIONAL",
+      country,
+      cityOrTown: getCustomField(
+        data,
+        "birth.child-view-group.internationalCityPrimaryChild"
+      ),
+    };
+  }
+  return {
+    addressType: "DOMESTIC",
+    country,
+    state: getCustomField(data, "birth.child-view-group.statePrimaryChild"),
+    district: getCustomField(
+      data,
+      "birth.child-view-group.districtPrimaryChild"
+    ),
+    cityOrTown: getCustomField(data, "birth.child-view-group.cityPrimaryChild"),
+    addressLine1: getCustomField(
+      data,
+      "birth.child-view-group.addressLine1PrimaryChild"
+    ),
+    addressLine2: getCustomField(
+      data,
+      "birth.child-view-group.addressLine2PrimaryChild"
+    ),
+    addressLine3: getCustomField(
+      data,
+      "birth.child-view-group.addressLine3PrimaryChild"
+    ),
+    postcodeOrZip: getCustomField(
+      data,
+      "birth.child-view-group.postalCodePrimaryChild"
+    ),
+  };
+}
+
+export const countryResolver = {
+  "child.nid": (data) => getIdentifier(data.child, "NATIONAL_ID"),
+  "child.address": (data) =>
+    convertChildAddress(data, data.eventLocation.address),
+  "child.attendantName": (data) =>
+    getCustomField(data, "birth.child.child-view-group.birthAttendantName"),
+  "child.attedantAtBirthId": (data) =>
+    getCustomField(data, "birth.child.child-view-group.birthAttendantId"),
+};
