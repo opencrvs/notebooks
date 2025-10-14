d66ddc86f374ee2b7bb686949b7ae460eb8b8478
Barry Dwyer <barry.dwyer@decodedev.co.za>
2025-10-14 15:16:32 +0200

    Added type safety and fixed informant id issue

--- v1-to-v2-data-migration/countryData/countryResolvers.ts
+++ v1-to-v2-data-migration/countryData/countryResolvers.ts
@@ -1,37 +1,65 @@
-import { getCustomField } from "../helpers/resolverUtils.ts";
+import { getCustomField } from '../helpers/resolverUtils.ts'
+import { EventRegistration } from '../helpers/types.ts'
 
 export const countryResolver = {
-'child.birthTime': (data) => getCustomField(
+  'child.birthTime': (data: EventRegistration) =>
+    getCustomField(data, 'birth.child.child-view-group.birthTime'),
+  'child.iD': (data: EventRegistration) => data.child?.identifier?.[0]?.id,
+  'child.legacyBirthRegistrationNumber': (data: EventRegistration) =>
+    getCustomField(
       data,
-      'birth.child.child-view-group.birthTime'
+      'birth.child.child-view-group.legacyBirthRegistrationNumber'
     ),
-'child.iD': (data) => data.child.identifier[0].id,
-'child.legacyBirthRegistrationNumber': (data) => getCustomField(data, 'birth.child.child-view-group.legacyBirthRegistrationNumber'),
-'child.legacyBirthRegistrationDate': (data) => getCustomField(data, 'birth.child-view-group.legacyBirthRegistrationDate'),
-'child.legacyBirthRegistrationTime': (data) => getCustomField(data, 'birth.child.child-view-group.legacyBirthRegistrationTime'),
-'informant.customizedExactDateOfBirthUnknown': (data) =>  getCustomField(data, 'birth.informant-view-group.customizedExactDateOfBirthUnknown'),
-'informant.yearOfBirth': (data) => getCustomField(data, 'birth.informant-view-group.yearOfBirth'),
-'informant.iD': (data) => Number(data.informant.id),
-'informant.birthPlace': (data) => getCustomField(data, 'birth.informant.informant-view-group.birthPlace'),
-'informant.occupation': (data) => data.informant.occupation,
-'mother.motherIsDeceased': (data) => getCustomField(data, 'birth.mother-view-group.motherIsDeceased'),
-/* 'mother.address.streetLevelDetails.fokontanyCustomAddress': (data) => getCustomField(data,'birth.mother.mother-view-group.fokontanyCustomAddress'), */
-'mother.customizedExactDateOfBirthUnknown': (data) => getCustomField(data, 'birth.mother-view-group.customizedExactDateOfBirthUnknown'),
-'mother.yearOfBirth': (data) => getCustomField(data, 'birth.mother-view-group.yearOfBirth'),
-'mother.iD': (data) => data.mother.identifier?.[0]?.id,
-'mother.birthPlace': (data) => getCustomField(
+  'child.legacyBirthRegistrationDate': (data: EventRegistration) =>
+    getCustomField(data, 'birth.child-view-group.legacyBirthRegistrationDate'),
+  'child.legacyBirthRegistrationTime': (data: EventRegistration) =>
+    getCustomField(
       data,
-      'birth.mother.mother-view-group.birthPlace'
+      'birth.child.child-view-group.legacyBirthRegistrationTime'
     ),
-'father.fatherIsDeceased': (data) => getCustomField(
+  'informant.customizedExactDateOfBirthUnknown': (data: EventRegistration) =>
+    getCustomField(
       data,
-      'birth.father.father-view-group.fatherIsDeceased'
+      'birth.informant-view-group.customizedExactDateOfBirthUnknown'
     ),
-'father.fatherHasFormallyRecognisedChild': (data) => getCustomField(data, 'birth.father.father-view-group.fatherHasFormallyRecognisedChild'),
-'father.customizedExactDateOfBirthUnknown': (data) => getCustomField(data, 'birth.father-view-group.customizedExactDateOfBirthUnknown'),
-'father.yearOfBirth': (data) => getCustomField(data, 'birth.father-view-group.yearOfBirth'),
-'father.iD': (data) => data.father.identifier?.[0]?.id,
-'father.birthPlace': (data) => getCustomField(data, 'birth.father.father-view-group.birthPlace'),
+  'informant.yearOfBirth': (data: EventRegistration) =>
+    getCustomField(data, 'birth.informant-view-group.yearOfBirth'),
+  'informant.iD': (data: EventRegistration) =>
+    Number(data.informant?.identifier?.[0]?.id),
+  'informant.birthPlace': (data: EventRegistration) =>
+    getCustomField(data, 'birth.informant.informant-view-group.birthPlace'),
+  'informant.occupation': (data: EventRegistration) =>
+    data.informant?.occupation,
+  'mother.motherIsDeceased': (data: EventRegistration) =>
+    getCustomField(data, 'birth.mother-view-group.motherIsDeceased'),
+  /* 'mother.address.streetLevelDetails.fokontanyCustomAddress': (data) => getCustomField(data,'birth.mother.mother-view-group.fokontanyCustomAddress'), */
+  'mother.customizedExactDateOfBirthUnknown': (data: EventRegistration) =>
+    getCustomField(
+      data,
+      'birth.mother-view-group.customizedExactDateOfBirthUnknown'
+    ),
+  'mother.yearOfBirth': (data: EventRegistration) =>
+    getCustomField(data, 'birth.mother-view-group.yearOfBirth'),
+  'mother.iD': (data: EventRegistration) => data.mother?.identifier?.[0]?.id,
+  'mother.birthPlace': (data: EventRegistration) =>
+    getCustomField(data, 'birth.mother.mother-view-group.birthPlace'),
+  'father.fatherIsDeceased': (data: EventRegistration) =>
+    getCustomField(data, 'birth.father.father-view-group.fatherIsDeceased'),
+  'father.fatherHasFormallyRecognisedChild': (data: EventRegistration) =>
+    getCustomField(
+      data,
+      'birth.father.father-view-group.fatherHasFormallyRecognisedChild'
+    ),
+  'father.customizedExactDateOfBirthUnknown': (data: EventRegistration) =>
+    getCustomField(
+      data,
+      'birth.father-view-group.customizedExactDateOfBirthUnknown'
+    ),
+  'father.yearOfBirth': (data: EventRegistration) =>
+    getCustomField(data, 'birth.father-view-group.yearOfBirth'),
+  'father.iD': (data: EventRegistration) => data.father?.identifier?.[0]?.id,
+  'father.birthPlace': (data: EventRegistration) =>
+    getCustomField(data, 'birth.father.father-view-group.birthPlace'),
 }
 
 /* 
@@ -41,6 +69,6 @@ export const countryResolver = {
 'birth.child-view-group.otherPlaceOfBirthAddress' : 'child.address.other', */
 /* 'birth.child-view-group.nuiGeneratorError' : '', */
 /* 'birth.child-view-group.iDManual' : '', */
-/* 'birth.informant-view-group.fokontanyCustomAddress' : 'informant.address.streetLevelDetails.fokontanyCustomAddress',  */  
+/* 'birth.informant-view-group.fokontanyCustomAddress' : 'informant.address.streetLevelDetails.fokontanyCustomAddress',  */
 /* 'birth.father-view-group.fokontanyCustomAddress' : 'father.address.streetLevelDetails.fokontanyCustomAddress' */
 /* 'birth.documents-view-group.uploadDocForRecognition' : '' */
