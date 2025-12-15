4204a9943c0f1ed2d949986a9dd626740114e8e2
Barry Dwyer <barry.dwyer@decodedev.co.za>
2025-12-15 09:38:26 +0200

    Fix routes

--- v1-to-v2-data-migration/helpers/routes.ts
+++ v1-to-v2-data-migration/helpers/routes.ts
@@ -1,22 +1,22 @@
 import { DOMAIN } from './vars.ts'
 
-export const CONFIG = `https://${
+export const CONFIG = `http://${
   DOMAIN.includes('localhost') ? `${DOMAIN}:2021` : `config.${DOMAIN}`
 }`
-export const GATEWAY = `https://${
+export const GATEWAY = `http://${
   DOMAIN.includes('localhost') ? `${DOMAIN}:7070` : `gateway.${DOMAIN}`
 }`
 export const AUTH = `http://${
-  DOMAIN.includes('localhost') ? `${DOMAIN}:7070` : `auth.${DOMAIN}`
+  DOMAIN.includes('localhost') ? `${DOMAIN}:4040` : `auth.${DOMAIN}`
 }`
-export const COUNTRY_CONFIG = `https://${
+export const COUNTRY_CONFIG = `http://${
   DOMAIN.includes('localhost') ? `${DOMAIN}:3040` : `countryconfig.${DOMAIN}`
 }`
 
-export const REGISTER_APP = `https://${
+export const REGISTER_APP = `http://${
   DOMAIN.includes('localhost') ? `${DOMAIN}:3000` : `register.${DOMAIN}`
 }`
 
-export const API = `https://${
+export const API = `http://${
   DOMAIN.includes('localhost') ? `${DOMAIN}:3000/api` : `${DOMAIN}/api`
 }`
